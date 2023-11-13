1f1caef7ab32a797a3e349dbcd3b202d6d69f794
mustafa iram <mustafa.iram.ext1@holcim.com>
2023-11-13 09:16:09 +0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'google-photorealistic-3d',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
